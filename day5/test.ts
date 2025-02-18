2f37534a7a1651dd09d158e561a6d4cf6034bff8
eungsu <eungsu1217@gmail.com>
2025-02-18 21:04:16 +0900

    done

--- day5/test.ts
+++ day5/test.ts
@@ -1,15 +1,48 @@
-type Commend = "add" | "substract" | "multiply" | "divide" | "remainder";
-type Direction = "up" | "down" | "right" | "left";
+{
+  // type CoffeeCup = {
+  //   shots: number;
+  //   hasMilk: boolean;
+  // };
 
-// 계산기 만들기 객체지향
+  //   const COFFEE_GRAMM_PER_SHOT: number = 7;
+  //   let coffeeBeans: number = 0;
+  //   function makeCoffee(shots: number): CoffeeCup {
+  //     if (coffeeBeans > shots * COFFEE_GRAMM_PER_SHOT) {
+  //       throw new Error("not enough coffee beans");
+  //     }
+  //     coffeeBeans -= shots * COFFEE_GRAMM_PER_SHOT;
+  //     return {
+  //       shots: shots,
+  //       hasMilk: false,
+  //     };
+  //   }
+  //   const coffee = makeCoffee(2);
+  //   console.log(coffee);
 
-class Calculate {
-  a: number;
-  b: number;
-  constructor(a: number, b: number) {
-    this.a = a;
-    this.b = b;
-  }
-
-  calculate = () => {};
+  // class CoffeeMachine {
+  //   shots: number;
+  //   hasMilk: boolean;
+  //   coffeeBeans: number = 50;
+  //   COFFEE_GRAMM_PER_SHOT: number = 7;
+  //   constructor(shots: number, hasMilk: boolean) {
+  //     this.shots = shots;
+  //     this.hasMilk = hasMilk;
+  //   }
+  //   makeCoffee(shots: number): CoffeeCup {
+  //     if (this.coffeeBeans < shots * this.COFFEE_GRAMM_PER_SHOT) {
+  //       throw new Error("not enough coffee");
+  //     }
+  //     this.coffeeBeans -= shots * this.COFFEE_GRAMM_PER_SHOT;
+  //     console.log(this.coffeeBeans);
+  //     return {
+  //       shots,
+  //       hasMilk: false,
+  //     };
+  //   }
+  // }
+  // let newCoffee = new CoffeeMachine(2, false);
+  // let coffees = newCoffee.makeCoffee(2);
+  // console.log(coffees);
+  let str = "hhsafsadf.sdafsdaf";
+  console.log(str.split(".")[1]);
 }
